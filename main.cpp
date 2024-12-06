3fb9a603fecd6281dca34c406b48636dab119778
yesmine <Yesmine.Kacem@esprit.tn>
2024-12-06 10:04:25 +0100

    4eme commit from GestionServices sur main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include <iostream>
-using namespace std;
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include "connection.h"
+#include <QString>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    Connection c;
+    bool test=c.createconnect();
+    if(test)
+    {w.show();
+        QMessageBox::information(nullptr, QObject::tr("database is open"),
+                                 QObject::tr("connection successful.\n"
+                                             "Click Cancel to exit."), QMessageBox::Cancel);
 
-int main() {
-    cout << "hey" << endl;
-    return 0;
+    }
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                              QObject::tr("connection failed.\n"
+                                          "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+    return a.exec();
 }
